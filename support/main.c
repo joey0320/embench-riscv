948bc3cd7cf56fd0d2c52ae1b4d852de5d44c4ea
joey0320 <joonho0320@gmail.com>
2023-10-04 16:50:44 -0700

    IPC riscv

--- support/main.c
+++ support/main.c
@@ -12,6 +12,10 @@
    SPDX-License-Identifier: GPL-3.0-or-later */
 
 #include "support.h"
+#include "encoding.h"
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdio.h>
 
 
 int __attribute__ ((used))
@@ -22,17 +26,24 @@ main (int argc __attribute__ ((unused)),
   volatile int result;
   int correct;
 
+
   initialise_board ();
   initialise_benchmark ();
   warm_caches (WARMUP_HEAT);
 
+  uint64_t start_cycle = rdcycle();
+  uint64_t start_inst = rdinstret();
   start_trigger ();
   result = benchmark ();
   stop_trigger ();
+  uint64_t end_inst = rdinstret();
+  uint64_t end_cycle = rdcycle();
 
   /* bmarks that use arrays will check a global array rather than int result */
 
   correct = verify_benchmark (result);
+
+  printf("Cycles: %" PRIu64 " Insts: %" PRIu64 "\n", end_cycle - start_cycle, end_inst - start_inst);
 
   return (!correct);
 
